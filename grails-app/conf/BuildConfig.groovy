1bfa24e408504ccf656fc770475e416d6861417e
Burt Beckwith <beckwithb@vmware.com>
2012-11-29 14:39:16 -0500

    changed from depending on the hibernate plugin to just its jars

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -21,6 +21,22 @@ grails.project.dependency.resolution = {
 						'slf4j-log4j12', 'text'
 		}
 
+		compile('org.hibernate:hibernate-core:3.6.10.Final') {
+			excludes 'ant', 'antlr', 'cglib', 'commons-collections', 'commons-logging', 'commons-logging-api',
+			         'dom4j', 'h2', 'hibernate-commons-annotations', 'hibernate-jpa-2.0-api', 'hibernate-validator',
+			         'javassist', 'jaxb-api', 'jaxb-impl', 'jcl-over-slf4j', 'jboss-jacc-api_JDK4', 'jta',
+			         'junit', 'slf4j-api', 'slf4j-log4j12', 'validation-api'
+		}
+
+		compile( 'org.hibernate:hibernate-commons-annotations:3.2.0.Final' ){
+			excludes 'commons-logging', 'commons-logging-api', 'jcl-over-slf4j', 'junit', 'slf4j-api', 'slf4j-log4j12'
+		}
+
+		runtime('dom4j:dom4j:1.6.1') {
+			excludes 'jaxen', 'jaxme-api', 'junitperf', 'pull-parser', 'relaxngDatatype', 'stax-api',
+			         'stax-ri', 'xalan', 'xercesImpl', 'xpp3', 'xsdlib', 'xml-apis'
+		}
+
 		compile 'freemarker:freemarker:2.3.8'
 
 		compile 'org.hibernate:jtidy:r8-20060801'
@@ -29,8 +45,6 @@ grails.project.dependency.resolution = {
 	plugins {
 
 		compile ':app-info:1.0.2'
-
-		runtime ":hibernate:$grailsVersion"
 
 		build(':release:2.1.0', ':rest-client-builder:1.0.2') {
 			export = false
